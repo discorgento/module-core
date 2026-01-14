a3cdd8b4a6f9a901011cbf2e10c8e1ce9cf209c2
Ivan Augusto <4603111+ivanaugustobd@users.noreply.github.com>
2026-01-14 19:17:43 -0300

    review: php 8.3 support

--- Helper/Context/Data.php
+++ Helper/Context/Data.php
@@ -6,12 +6,6 @@ namespace Discorgento\Core\Helper\Context;
 use Magento\Framework\App\Helper\Context as NativeContext;
 use Magento\Framework\App\State;
 
-/**
- * This custom context allow extending the original helper
- * without the need to repeat every original dependency on it,
- * just like Magento native abstract helper ヽ(´ー｀)ノ
- * @see \Magento\Framework\App\Helper\AbstractHelper
- */
 final class Data
 {
     /** @var NativeContext */
@@ -20,6 +14,7 @@ final class Data
     /** @var State */
     public $state;
 
+    // phpcs:ignore
     public function __construct(
         NativeContext $nativeContext,
         State $state
